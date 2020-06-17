53022271d4410508070bd8922ab4a1058f99550d
Johannes Westhuis <johannes.westhuis@gmail.com>
2020-06-17 21:18:22 +0200

    allow additional dependencies to be installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 REF=$1
+PGKS=$2
+
 CPWD=`pwd`
 NPATH=/tmp/nginx
 if [ -d $NPATH ]; then
@@ -15,6 +17,12 @@ if [ -n $REF ]; then
   git checkout $REF
 fi
 
+if [ -n $PGKS ]; then
+  apt install -y $PGKS
+fi
+
 ./auto/configure --with-compat --without-http_gzip_module --without-http_rewrite_module --add-dynamic-module=$CPWD
 make modules
-
+if [ $? -ne 0 ]; then
+  exit 1;
+fi
