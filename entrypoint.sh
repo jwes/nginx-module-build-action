710f6d904958fc5e3b4f80a0e27d331dee1d382f
Johannes Westhuis <johannes.westhuis@gmail.com>
2020-06-20 21:08:18 +0200

    copy resulting shared libraries to workdir
    also: abort on errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,14 +15,24 @@ fi
 cd $NPATH
 if [ -n $REF ]; then
   git checkout $REF
+  if [ $? -ne 0 ]; then
+    exit 1;
+  fi
 fi
 
 if [ -n $PGKS ]; then
   apt install -y $PGKS
+  if [ $? -ne 0 ]; then
+    exit 1;
+  fi
 fi
 
 ./auto/configure --with-compat --without-http_gzip_module --without-http_rewrite_module --add-dynamic-module=$CPWD
+if [ $? -ne 0 ]; then
+  exit 1;
+fi
 make modules
 if [ $? -ne 0 ]; then
   exit 1;
 fi
+find . -name *.so -exec cp {} $CPWD \;
